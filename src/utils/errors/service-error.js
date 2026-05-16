a9da16eb050c420b6badbbf64b27a4c9d4af1515
Harshita Patel <get2harshitad@gmail.com>
2026-05-16 23:39:36 +0530

    Added error logs

---
+++ src/utils/errors/service-error.js
@@ -0,0 +1,17 @@
+const {StatusCodes} = require('http-status-codes');
+
+class ServiceError extends Error{
+
+    constructor(
+        message = 'Something went wrong in the service layer', 
+        explanation = 'Service Layer Error', 
+        statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+    ){
+        this.name = 'ServiceError';
+        this.message = message;
+        this.explanation = explanation;
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = ServiceError;
